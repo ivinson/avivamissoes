abd0bf37ddcebf80d7be084a656345e3b70dbeeb
Debora Silva <deboraro.61@gmail.com>
2024-07-02 19:19:56 -0300

    subindo a biblioteca phpspreadsheet e adicionada ela ao upload-extrato

--- upload-extrato.php
+++ upload-extrato.php
@@ -11,10 +11,6 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 //include('db.php'); 
 $pasta = "retorno/extratos/"; /* formatos de imagem permitidos */
 
